b7744dc4a3ffa7283009be9c27594ef15e8ec0b1
oliaivanova <o.ivanova@fbs.com>
2022-09-17 17:43:38 +0200

    test commit

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
-import { Image, View } from 'react-native';
+import {Image, ScrollView, Text, View} from 'react-native';
 
 export const Home: FC = () => {
     return (
-        <View>
+        <ScrollView>
             <Image
                 source={{
                     uri: 'https://www.rover.com/blog/wp-content/uploads/2020/10/Miss_Dog-Mom_Harvey_3-smaller.jpg',
@@ -11,6 +11,14 @@ export const Home: FC = () => {
                 style={{ width: '100%', height: 500 }}
                 resizeMode="cover"
             />
-        </View>
+            <Text>Home</Text>
+            <Image
+                source={{
+                    uri: 'https://i.pinimg.com/736x/db/5d/30/db5d30256add2118cb2dc75ee6bcbb86.jpg',
+                }}
+                style={{ width: '100%', height: 500 }}
+                resizeMode="cover"
+            />
+        </ScrollView>
     );
 };
